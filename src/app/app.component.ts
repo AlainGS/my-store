8b6ae3ebe8682c3b2864f2c9a5011a665da2da75
AlainGS <i201921039@cibertec.edu.pe>
2023-09-09 01:07:29 -0500

    faltantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { Person, Producto } from './recap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-store';
+  name = 'Alain';
+  age = 38;  
+
+  persona = new Person(38,'Gonzales');
+  personaEdad : number = this.persona.edad();
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+
+  btnDisabled = true;
+  isVisibleModal = false;
+  
+  product : Producto;
+  constructor(){
+    this.product = new Producto(1,'Laptop', 1025.99);
+    
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.personaEdad = this.personaEdad+1;
+  }
+
+  showModal(){
+    this.isVisibleModal = true;
+  }
+
+  hideModal(){
+    this.isVisibleModal = false;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.persona.lastname = element.value;
+
+  }
+
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,42 @@
+const username = 'alaking';
+const names: string = 'alain';
+const num = 2;
+const num1 : number = 3;
+
+const suma = (a: number, b: number) => {
+  return a + b;
+}
+suma(1,2);
+
+export class Person{
+  private age: number;
+  public lastname: string;
+
+  constructor(edad: number, apellido: string){
+    this.age = edad;
+    this.lastname = apellido;
+  }
+
+  public edad(): number{
+    return this.age;
+  }
+}
+const alain = new Person(38,'Gonzales');
+const edad : number = alain.edad();
+
+class Persona{
+  constructor(
+    public a: number,
+    public ln: string
+  ){}
+}
+const laura = new Persona(40, 'Benites');
+laura.ln
+
+export class Producto{  
+  constructor(
+    public id: number, 
+    public descripcion: string, 
+    public precio: number
+  ){}
+}
